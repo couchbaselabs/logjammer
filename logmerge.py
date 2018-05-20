0fa87ab6efc24cba9b52d2ab1d013a4ae0a77f57
Steve Yen <steve.yen@gmail.com>
2018-05-20 11:36:49 -0700

    use argparse

--- logmerge.py
+++ logmerge.py
@@ -3,6 +3,7 @@
 
 from dateutil import parser
 
+import argparse
 import glob
 import heapq
 import os
@@ -11,43 +12,46 @@ import sys
 
 
 def main(argv):
-    """logmerge merges log file entries by timestamp to stdout
+    ap = argparse.ArgumentParser(
+       description='merges entries from log files by timestamp to stdout',
+       epilog="""An entry in a log file may take more than one line,
+where entries are determined via heuristic timestamp parsing.  The log
+file entries in each log file must be sorted by timestamp, as logmerge
+operates by performing a heap merge.""")
 
-Example usage:
+    ap.add_argument('--suffix', type=str, default=".log",
+                    help="""when expanding directory paths,
+                    find log files that have this glob suffix""")
+    ap.add_argument('--max_lines_per_entry', type=int, default=100,
+                    help="""max number of lines in an entry before clipping""")
+    ap.add_argument('path', nargs='*',
+                    help="""log file or directory of log files""")
 
-  ./logmerge.py this.log that.log other.log
-  ./logmerge.py path/dirA path/dirB one.log two.log
+    args = ap.parse_args(argv[1:])
 
-The entries in each log file must already be sorted by timestamp, as
-logmerge operates by performing a heap merge.
-"""
-    # When expanding a directory, log files have this suffix.
-    glob_suffix = "/*.log"
+    process(args.path,
+            glob_suffix="/*" + args.suffix,
+            max_lines_per_entry=args.max_lines_per_entry)
 
-    seeks = None         # Optional dict of path => seek() positions.
-    max_entry_len = 100  # Entries that are too long aren't entries.
-    invert = False       # Emit non-entries instead of entries.
 
+def process(paths,
+            glob_suffix="/*.log",
+            max_lines_per_entry=100,  # Entries that are too long are clipped.
+            seeks=None):              # dict[path] => seek() positions.
     # Find log files.
-
-    paths = expand_paths(argv[1:], glob_suffix=glob_suffix)
+    paths = expand_paths(paths, glob_suffix)
 
     # Prepare heap entry for each log file.
-
-    heap_entries = prepare_heap_entries(paths,
-                                        seeks=seeks,
-                                        max_entry_len=max_entry_len,
-                                        invert=invert)
+    heap_entries = prepare_heap_entries(
+       paths,
+       max_lines_per_entry=max_lines_per_entry,
+       seeks=seeks)
 
     # Consume heap, emitting each heap entry.
-
-    path_prefix = os.path.commonprefix(paths)
-
-    print_heap_entries(path_prefix, heap_entries,
-                       max_entry_len=max_entry_len, invert=invert)
+    print_heap_entries(os.path.commonprefix(paths), heap_entries)
 
 
-def expand_paths(paths, glob_suffix="/*.log"):
+def expand_paths(paths, glob_suffix):
     rv = []
 
     for path in paths:
@@ -60,14 +64,13 @@ def expand_paths(paths, glob_suffix="/*.log"):
 
 
 def prepare_heap_entries(paths,
-                         seeks=None,
-                         max_entry_len=100,
-                         invert=False):
+                         max_lines_per_entry=100,
+                         seeks=None):
     heap_entries = []
 
     for path in paths:
         f = open(path, 'r')
-        r = EntryReader(f, path, max_entry_len)
+        r = EntryReader(f, path, max_lines_per_entry)
 
         if seeks:
             seek_to = seeks.get(path)
@@ -76,7 +79,7 @@ def prepare_heap_entries(paths,
                 r.read()  # Discard as it's in the middle of a entry.
 
         entry = r.read()
-        if entry_ok(entry, max_entry_len, invert):
+        if entry:
             heap_entries.append((parse_entry_timestamp(entry[0]), entry, r))
 
     heapq.heapify(heap_entries)
@@ -84,33 +87,24 @@ def prepare_heap_entries(paths,
     return heap_entries
 
 
-def entry_ok(entry, max_entry_len, invert):
-    if entry and len(entry) >= 1:
-        if len(entry) <= max_entry_len:
-            return not invert
-        else:
-            return invert
-
-
-def print_heap_entries(path_prefix, heap_entries,
-                       max_entry_len=100, invert=False):
+def print_heap_entries(path_prefix, heap_entries):
     while heap_entries:
         timestamp, entry, r = heapq.heappop(heap_entries)
 
         print r.path[len(path_prefix):], "".join(entry)
 
         entry = r.read()
-        if entry_ok(entry, max_entry_len, invert):
+        if entry:
             heapq.heappush(heap_entries,
                            (parse_entry_timestamp(entry[0]), entry, r))
 
 
 class EntryReader(object):
-    def __init__(self, f, path, max_entry_len):
+    def __init__(self, f, path, max_lines_per_entry):
         self.f = f
         self.path = path
         self.last_line = None
-        self.max_entry_len = max_entry_len
+        self.max_lines_per_entry = max_lines_per_entry
 
     def read(self):
         entry = []
@@ -127,7 +121,8 @@ class EntryReader(object):
             if parse_entry_timestamp(self.last_line):
                 return entry
 
-            entry.append(self.last_line)
+            if len(entry) < self.max_lines_per_entry:
+                entry.append(self.last_line)
 
 
 # Non-whitespace chars followed by "YYYY-MM-DDThh:mm:ss.sss".
