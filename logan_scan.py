99a2291cff25c9fee05f3013543567deae923457
Steve Yen <steve.yen@gmail.com>
2018-06-17 16:06:31 -0700

    identifier ##a from projector.log

--- logan_scan.py
+++ logan_scan.py
@@ -456,6 +456,7 @@ pattern_num_ish = [
     ("#hms", r"T?\d\d:\d\d:\d\d"),
     ("#ip4", r"\d+\.\d+\.\d+\.\d+"),
     ("#idn", r"[a-zA-Z][a-zA-Z\-_]+\d+"),  # Numbered identifier, like "vb8".
+    ("#idh", r"##[a-f0-9]+"),
     ("#neg", r"-\d[\d\.]*"),               # Negative dotted number.
     ("#pos", r"\d[\d\.]*")]                # Positive dotted number.
 
